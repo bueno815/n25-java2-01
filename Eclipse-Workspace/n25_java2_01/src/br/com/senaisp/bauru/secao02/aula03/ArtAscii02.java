82fca33722370efd00f6ed7008bf4f50029ece6f
gizelle <gizellebueno@bol.com.br>
2025-07-30 21:50:10 -0300

    cobra

---
+++ Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao02/aula03/ArtAscii02.java
@@ -0,0 +1,27 @@
+package br.com.senaisp.bauru.secao02.aula03;
+
+public class ArtAscii02 {
+
+	public static void main(String[] args) {
+System.out.println("    ,==;,\r\n"
+		+ "       )a,a\\g\r\n"
+		+ "       \\=_/8\r\n"
+		+ "       _| (_3,\r\n"
+		+ "      /(__/\\]\\\r\n"
+		+ "     (_,,__) \\\\\r\n"
+		+ "     //\\  ;/  \\\\ \r\n"
+		+ "    //  )__\\   \\|_\r\n"
+		+ "  _'/  |[]__L,  ,>}\r\n"
+		+ " /t}  / ,   [| \r\n"
+		+ "6    /-.|=._|/\r\n"
+		+ "    /  .'`-/`\r\n"
+		+ "   ( .' | /\r\n"
+		+ "   \\ |  ( |\r\n"
+		+ "    \\_)  \\_).\r\n"
+		+ "     \\ \\  \\ |\r\n"
+		+ "      \\ >  >|\r\n"
+		+ " snd /.'  / /\r\n"
+		+ "         '-'");
+	}
+
+}
