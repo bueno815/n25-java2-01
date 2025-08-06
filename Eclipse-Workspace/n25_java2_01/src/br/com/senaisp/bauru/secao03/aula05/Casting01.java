c9a7d6672e54257fcccb4909b2db4a30c5171de8
gizelle bueno <gizellebueno@bol.com.br>
2025-08-05 21:50:23 -0300

    SAO PAULO

--- Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao03/aula05/Casting01.java
+++ Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao03/aula05/Casting01.java
@@ -12,26 +12,7 @@ public class Casting01 {
 		bVlr++;//iria para 128, mas o limite do byte é -128 á 127
 		System.out.println("Isso vai extrapolar o limite do byte: " + bVlr);
 		
-		 //World population today
-        long currentWorldPop = 7_000_000_000;
-        System.out.println("Current World Population: " +currentWorldPop);
-        
-        //World population in 1950,  Source: United Nations Population Division
-        //2.52 Billion
-        int africaPop   =   221_000_000;
-        int asiaPop     = 1_402_000_000;
-        int europePop   =   547_000_000;
-        int americasPop =   339_000_000;
-        int oceanaPop   =    13_000_000;
-        System.out.println("World Population in 1950: " 
-                +africaPop +asiaPop +europePop +americasPop +oceanaPop);
-        
-        //The current population of Asia is an estimated 60% of the world population
-        double percentAsia = 0.6;
-        long currentAsiaPop = currentWorldPop * percentAsia;
-        System.out.println("Current Asia Population: " +currentAsiaPop);      
-       
-
+		
 	}
 
 }
