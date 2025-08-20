a7d6e3a4857aefb42f0145677a9977a1af65e72b
gizelle bueno <gizellebueno@bol.com.br>
2025-08-19 21:45:35 -0300

    AULA 13

--- Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao08/aula13/BlackJackPlayer.java
+++ Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao08/aula13/BlackJackPlayer.java
@@ -45,5 +45,12 @@ public class BlackJackPlayer {
 	public ArrayList<Carta> getCartas() {
 		return cartas;
 	}
+	public int getTotalCarta() {
+		int tot = 0;
+		for(Carta ct : cartas) {
+			tot += ct.getValor();
+		}
+		return tot;
+	}
 
 }
